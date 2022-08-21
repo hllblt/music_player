f945c795c739cae0ca592392a97929d7959f496c
Halil Bulut <halill.bulutt.1@gmailcom>
2022-08-21 19:30:08 +0200

    added rotate effect to cover

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const playButton = document.getElementById("playButton");
 const pauseButton = document.getElementById("pauseButton");
+const cover = document.getElementsByClassName("cover")[0];
 
-// playButton.addEventListener("click", () => {
-//     playButton.style.opacity = "0";
-//     pauseButton.style.opacity = "1";
-// });
 let isPlaying = false;
+let intervalCounter = 1;
+
+// const myInterval = setInterval(() => {
+//     cover.style.transform = `rotate(${intervalCounter*45}deg)"`;
+//     intervalCounter += 1
+//  }, 1000);
 
 playButton.addEventListener("click", () => {
     if (isPlaying) {
+        clearInterval(myInterval);
         playButton.innerHTML = '<i class="fa-solid fa-play"></i>';
-
+        console.log(isPlaying);
         isPlaying = !isPlaying;
     } else {
-        isPlaying = !isPlaying;
+        myInterval = setInterval(() => {
+            cover.style.transform =
+                "rotate(" + `${intervalCounter * 0.5}` + "deg)";
+            intervalCounter += 1;
+            // console.log("Interval");
+        }, 10);
         playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+        // console.log(isPlaying);
+        isPlaying = !isPlaying;
     }
 });
