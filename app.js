dd226ac83401c818a429097f205f45a26386c3ab
Halil Bulut <halill.bulutt.1@gmailcom>
2022-08-23 20:52:04 +0200

    added some function to prev, next, play and pause buttons

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const prevSongButton = document.getElementById("prevSongButton");
 const songTitle = document.getElementById("songTitle");
 // const display = document.getElementsByClassName("display")[0];
 
+const happy = document.getElementById("happy");
+console.log(happy);
+
 const musicInfos = [
     {
         img: "https://imgyukle.com/f/2022/08/21/nyAJFN.jpg",
@@ -49,28 +52,21 @@ let intervalCounter = 1;
 let songNumber = 0;
 let isFirstPlay = true;
 
-// const myInterval = setInterval(() => {
-//     cover.style.transform = `rotate(${intervalCounter*45}deg)"`;
-//     intervalCounter += 1
-//  }, 1000);
-
 playButton.addEventListener("click", () => {
-    if (isPlaying) {
-        clearInterval(myInterval);
-        playButton.innerHTML = '<i class="fa-solid fa-play"></i>';
-        console.log(isPlaying);
-        isPlaying = !isPlaying;
-    } else {
-        myInterval = setInterval(() => {
-            cover.style.transform =
-                "rotate(" + `${intervalCounter * 0.2}` + "deg)";
-            intervalCounter += 1;
-            // console.log("Interval");
-        }, 10);
-        playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
-        // console.log(isPlaying);
-        isPlaying = !isPlaying;
-    }
+    // if (isPlaying) {
+    //     clearInterval(myInterval);
+    //     playButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+    //     isPlaying = !isPlaying;
+    // } else {
+    //     myInterval = setInterval(() => {
+    //         cover.style.transform =
+    //             "rotate(" + `${intervalCounter * 0.2}` + "deg)";
+    //         intervalCounter += 1;
+    //     }, 10);
+    //     playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    //     isPlaying = !isPlaying;
+    // }
+    playOrPause();
 });
 
 // in this part, we assigned next and prev buttons' functions. when we press
@@ -83,19 +79,82 @@ nextSongButton.addEventListener("click", () => {
         songNumber += 1;
     }
     songTitle.innerText = musicInfos[songNumber].songName;
-    cover.style.backgroundImage = "url(" + `${musicInfos[songNumber].img}` + ")";
+    cover.style.backgroundImage =
+        "url(" + `${musicInfos[songNumber].img}` + ")";
     cover.style.transform = "rotate(0deg)";
     intervalCounter = 0;
+    songPlayer(songNumber);
+    if (isPlaying) {
+        clearInterval(myInterval);
+        isPlaying = false;
+    }
+    playOrPause();
 });
+
 prevSongButton.addEventListener("click", () => {
-    // console.log("prev");
     if (songNumber === 0) {
         songNumber = 6;
     } else {
         songNumber -= 1;
     }
     songTitle.innerText = musicInfos[songNumber].songName;
-    cover.style.backgroundImage = "url(" + `${musicInfos[songNumber].img}` + ")";
+    cover.style.backgroundImage =
+        "url(" + `${musicInfos[songNumber].img}` + ")";
     cover.style.transform = "rotate(0deg)";
     intervalCounter = 0;
+    songPlayer(songNumber);
+    if (isPlaying) {
+        clearInterval(myInterval);
+        isPlaying = false;
+    }
+    playOrPause();
 });
+
+function songPlayer(songNum) {
+    // bu fonksiyon,next veya prev tuşlarına basıldığında çalan şarkıyı
+    // durdurup, yeni şarkıyı çalmak için yazıldı.
+
+    // for (let i = 0; i < musicInfos.length; i++) {
+    //     document.getElementById(musicInfos[i].url).pause();
+    //     document.getElementById(musicInfos[i].url).currentTime = 0;
+    // }
+
+    document
+        .getElementById(musicInfos[songNum == 0 ? 6 : songNum - 1].url)
+        .pause();
+    document.getElementById(
+        musicInfos[songNum == 0 ? 6 : songNum - 1].url
+    ).currentTime = 0;
+    document
+        .getElementById(musicInfos[songNum == 6 ? 0 : songNum + 1].url)
+        .pause();
+    document.getElementById(
+        musicInfos[songNum == 6 ? 0 : songNum + 1].url
+    ).currentTime = 0;
+
+    document.getElementById(musicInfos[songNum].url).play();
+}
+
+function playOrPause() {
+    if (isPlaying) {
+        clearInterval(myInterval);
+        playButton.innerHTML = '<i class="fa-solid fa-play"></i>';
+        document.getElementById(musicInfos[songNumber].url).pause();
+        isPlaying = !isPlaying;
+    } else {
+        myInterval = setInterval(() => {
+            cover.style.transform =
+                "rotate(" + `${intervalCounter * 0.2}` + "deg)";
+            intervalCounter += 1;
+        }, 10);
+        playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';
+        isPlaying = !isPlaying;
+        document.getElementById(musicInfos[songNumber].url).play();
+    }
+}
+
+// HATIRLATMA
+// play ya da pause tuşuna basıldığında çalışacak bir fonksiyon yazılacak.
+// bu fonksiyon aynı zamanda next ya da prev tuşuna basıldığında da
+// çalışacak. next ya da prev tuşuna basıldığında play tuşuna da basılmış
+// gibi algılayacak.
